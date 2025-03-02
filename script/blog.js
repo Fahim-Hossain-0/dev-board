61f903c231e008d0783b740f3481c91d16d41138
fahim-hossain-0 <fahimhossain735@gmail.com>
2025-03-02 23:19:24 +0600

    date

---
+++ script/blog.js
@@ -0,0 +1,3 @@
+document.getElementById("blog_back_btn").addEventListener("click", function () {
+    window.location.href = "/index.html";
+});
---
+++ script/date.js
@@ -0,0 +1,27 @@
+let today = new Date();
+
+let dayName = today.toLocaleString("en-US", { weekday: "long" });
+let monthName = today.toLocaleString("en-US", { month: "long" });
+let day = today.getDate();
+let year = today.getFullYear();
+
+
+
+const date = document.getElementById('date')
+
+const p = document.createElement('p')
+p.innerHTML = `${dayName} <br>${monthName} ${year}`
+// p1.innerText = `${dayName}`
+// const p2 = document.createElement('p')
+// p2.innerText = `${monthName}`
+// const p3 = document.createElement('p')
+// p3.innerText = `${year}`
+// date.appendChild(p1)
+// date.appendChild(p2)
+date.appendChild(p)
+
+const timeCards = document.querySelectorAll('.time-card')
+for(const timeCard of timeCards){
+    timeCard.innerHTML = `${day} ${monthName} ${year}`
+
+}
